623c3e47c789762979d9dfe4a45714b2285726c2
Navid Hasanzadeh <68333042+navidhasanzadeh@users.noreply.github.com>
2022-11-19 18:53:23 -0500

    adding structure and baseline model

--- model_baseline.py
+++ model_baseline.py
@@ -1,19 +1,44 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+import random
 
-# model_name = "pranavpsv/genre-story-generator-v2"
+model_name = "pranavpsv/genre-story-generator-v2"
 # model_name = "pranavpsv/gpt2-genre-story-generator"
-model_name = "Deniskin/gpt3_medium"
+# model_name = "openai-gpt"
+# model_name = "pranavpsv/gpt2-story-gen"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
+begin = ['A long time ago and far,', 'Once upon a time,']
+skeletons = ["","","","","But", "After a while,", "However,", "Then,", "So,", "In fact,", "Moreover", "In contrast,", "Consequently,", "Because of", "Furthermore"]
+end = ['Finally, ']
 
-text = '''Once upon a time there was a dear little girl'''
+# genre = "<action>"
+# genre = "<sci_fi>"
+genre = "<horror>"
 
-inputs  = tokenizer(text , return_tensors="pt")
+text = genre + " " + random.choice(begin)
+flag = True
+while flag:
+    with torch.no_grad():
+        # output = model.generate(**inputs, max_length=200, do_sample=True)
 
-with torch.no_grad():
-    output = model.generate(**inputs, max_length=200)
+        print(text)
+        print("---------------------")
+        inputs  = tokenizer(text , return_tensors="pt")
+        output = model.generate(**inputs, max_length=400,  do_sample=True)
+        new_generated = tokenizer.decode(output[0])[len(text):]
+        dot_point = new_generated.find('.')
+        text = text + new_generated[:dot_point+1] + ' '
+        for w in end:
+            if w in text:
+                flag = False
+        if flag==True:
+            if len(text.split(" "))>150:
+                text = text + random.choice(end)
+            else:
+                text = text + random.choice(skeletons)
+        
 
-print(tokenizer.decode(output[0]))
+print(text)
