8b8fa7cf0eb2af61a30ee74eedaf281ce6d89aad
Alan Du <torterra1999@hotmail.com>
2022-11-20 17:31:34 -0500

    added age to childrenstories.tsv

--- data/data_process.py
+++ data/data_process.py
@@ -19,7 +19,7 @@ def story_type_A(filename = "data\story_7.txt", output_filename = "data\processe
             else:
                 if len(line)>1:
                     story = story + lines[i].strip()
-    pd.DataFrame.from_dict(data=output, orient='index').to_csv(output_filename, header=False)
+    pd.DataFrame.from_dict(data=output, orient='index').to_csv(output_filename, header=False, sep='\t')
 
 def story_type_B(filename = "story_7.txt", output_filename = "processed_data/story_7.csv", start_line = 0):
     with open(filename, "r", encoding="utf8") as f:
@@ -64,6 +64,21 @@ def add_genre_to_text_data(filename = "processed_data\story_2.tsv"):
     
     data.to_csv(filename[:-4] + "_with_age_genre.tsv", sep='\t')
 
+def process_childrenstories():
+    filename="data\childrenstories.txt"
+    output_filename="data\processed_data/childrenstories.tsv"
+    numrows = 72
+
+    df = pd.read_table('childrenstories.tsv', nrows=numrows)
+    for i in range (numrows):
+        #strip whitespace
+        text = df['text'][i].strip()
+        df['text'][i] = text
+
+        #add age
+        df['age'] = round(process_age.calculate_grade(text))
+
+    df.to_csv("processed_data/childrenstories.tsv", sep='\t')
 
 add_genre_to_text_data("processed_data\story_3.tsv")
 
