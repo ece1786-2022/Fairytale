0b8020ca8c1e1c2e953d260571fece5116fc6ebc
Navid Hasanzadeh <68333042+navidhasanzadeh@users.noreply.github.com>
2022-11-19 19:16:59 -0500

    adding age and genre prediction for the baseline model

--- model_baseline.py
+++ model_baseline.py
@@ -1,6 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import random
+from data import process_age
+from data import process_genre
 
 model_name = "pranavpsv/genre-story-generator-v2"
 # model_name = "pranavpsv/gpt2-genre-story-generator"
@@ -14,9 +16,10 @@ begin = ['A long time ago and far,', 'Once upon a time,']
 skeletons = ["","","","","But", "After a while,", "However,", "Then,", "So,", "In fact,", "Moreover", "In contrast,", "Consequently,", "Because of", "Furthermore"]
 end = ['Finally, ']
 
-# genre = "<action>"
+genre = "<action>"
 # genre = "<sci_fi>"
-genre = "<horror>"
+# genre = "<horror>"
+# genre = "<drama>"
 
 text = genre + " " + random.choice(begin)
 flag = True
@@ -40,5 +43,11 @@ while flag:
             else:
                 text = text + random.choice(skeletons)
         
-
+print("=====================================================================")
 print(text)
+print("=====================================================================")
+print("Predicted genre: " + process_genre.get_genre(text.split(">")[1]))
+print("Predicted is_for_kids: " + str(process_age.is_for_kids(text.split(">")[1],
+                                                          levels_address = 
+                                                          'data/english_level/processed_words_level/')))
+print("Predicted age grade: " + str(round(process_age.calculate_grade(text.split(">")[1]))))
