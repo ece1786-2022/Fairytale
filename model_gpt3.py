c5442a21ad713edd2512d328568aa6f8e16c24f6
Alan Du <torterra1999@hotmail.com>
2022-11-20 20:53:42 -0500

    save outputs to text file for future use

--- model_gpt3.py
+++ model_gpt3.py
@@ -5,6 +5,7 @@ import numpy as np
 API_KEY = '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'
 orgID = "org-FamdhS456bzvQnGH0fUnw6aQ"
 URL = 'https://api.openai.com/v1/completions'
+filepath = "results/output.txt"
 
 ai.organization = orgID
 ai.api_key = API_KEY
@@ -18,29 +19,44 @@ starters = ['A long time ago and far,', 'Once upon a time,', 'There is a', 'I kn
 fillers = ['Suddenly', "But", "After a while,", "However,", "Then,", "So,", "In fact,", "Moreover", "In contrast,", "Consequently,", "Because of", "Furthermore"]
 closers = ['The time had come', 'It was finally time', 'Finally', 'In the end', 'After a while']
 genre = "fantasy"
-
 max_tokens = 200
 
+
+def run_model(responseprompt, max_tokens):
+    response = ai.Completion.create(model="text-davinci-002", prompt=responseprompt, temperature=0.7, top_p=1.0, max_tokens=max_tokens)
+    return response['choices'][0]['text']
+
+
+f = open(filepath, "a")
 for part in parts:
+    #create starting prompt
     startprompt = "Create the " + part + " of a " + genre + " story for children. The story should talk about " + values + "."
+    if(part == "beginning"):
+        story = starters[np.random.randint(0, len(starters))]
     responseprompt = startprompt + '\n' + story
 
-    print("============================PROMPT=================================\n")
-    print(responseprompt)
+    f.write("============================PROMPT=================================\n")
+    f.write(responseprompt + '\n')
 
-    response = ai.Completion.create(model="text-davinci-002", prompt=responseprompt, temperature=0.7, top_p=1.0, max_tokens=max_tokens)
-    text = response['choices'][0]['text']
-
+    #get the model output
+    text = run_model(responseprompt, max_tokens)
     max_tokens = len(tokenizer(text)['input_ids']) + 200
-
-    story = story + text
-    if(story == text):
-        if(part == "middle"):
-            story = story + text + fillers[np.random.randint(0, len(fillers))]
-
-    print("============================RESULT=================================\n")
-    print(story)
     
-# response = ai.Completion.create(model="text-davinci-002", prompt="Say this is a test", temperature=0, max_tokens=6)
+    f.write("............................RETURNED.................................\n")
+    f.write(text + '\n')
 
-# print(response['choices'][0]['text'])
+    #if the story has not changed, retry with a filler
+    story = story + text
+    if(story == text or text.replace('\n', '') == ''):
+        if(part == "middle"):
+            print("i'm here")
+            story = story + text + fillers[np.random.randint(0, len(fillers))]
+            responseprompt = startprompt + '\n' + story
+
+            text = run_model(responseprompt, max_tokens)
+            max_tokens = len(tokenizer(text)['input_ids']) + 200
+            story = story + text
+
+    f.write("----------------------------RESULT---------------------------------\n")
+    f.write(story + '\n')
+f.close()
